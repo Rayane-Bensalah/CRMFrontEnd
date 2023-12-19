d61f218f51e068c6c0e14575bbc343a85f3f6a34
MateoBrunetti <mateo.brunetti42@orange.fr>
2023-12-19 17:06:00 +0100

    created routes in services, updated constant base url

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {API_BASE_URL} from "../../app.constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  USER_BASE_URL = "users";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    return this.http.get(API_BASE_URL + this.USER_BASE_URL);
+  }
+
+  getUserById(id: number) {
+    return this.http.get(API_BASE_URL + this.USER_BASE_URL + '/' + id);
+  }
+
+  // postUser(user: User) {
+  //   return this.http.post(API_BASE_URL + this.USER_BASE_URL, user);
+  // }
+
+  deleteUser(id: number) {
+    return this.http.delete(API_BASE_URL + this.USER_BASE_URL + '/' + id);
+  }
+
+  // updateUser(user: User) {
+  //   return this.http.put(API_BASE_URL + this.USER_BASE_URL + '/'+user.id, user);
+  // }
+
+
 }

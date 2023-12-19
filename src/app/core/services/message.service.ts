6f8cd6fbeeca7304e2af29420032cea2ef8d8b08
MateoBrunetti <45964744+MateoBrunetti@users.noreply.github.com>
2023-12-19 17:21:35 +0100

    created routes in services, updated constant base url (#5)

--- src/app/core/services/message.service.ts
+++ src/app/core/services/message.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {API_BASE_URL} from "../../app.constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
+  MESSAGE_BASE_URL = "messages";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getMessages() {
+    return this.http.get(API_BASE_URL + this.MESSAGE_BASE_URL);
+  }
+
+  getMessageById(id: number) {
+    return this.http.get(API_BASE_URL + this.MESSAGE_BASE_URL + '/' + id);
+  }
+
+  // postMessage(message: Message) {
+  //   return this.http.post(API_BASE_URL + this.MESSAGE_BASE_URL, message);
+  // }
+
+  deleteMessage(id: number) {
+    return this.http.delete(API_BASE_URL + this.MESSAGE_BASE_URL + '/' + id);
+  }
+
+  // updateMessage(message: Message) {
+  //   return this.http.put(API_BASE_URL + this.MESSAGE_BASE_URL + '/'+message.id, message);
+  // }
+
 }

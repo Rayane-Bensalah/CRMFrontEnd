4260481458612d9a3acffd91306c93bbfafa963e
MateoBrunetti <mateo.brunetti42@orange.fr>
2023-12-20 15:06:35 +0100

    fetcher service fix

--- src/app/core/models/channel.model.ts
+++ src/app/core/models/channel.model.ts
@@ -1,4 +1,4 @@
-import {Message} from "./message.model";
+import { Message } from "./message.model";
 
 /**
  * Model for entity Channel
--- src/app/core/services/channel.service.ts
+++ src/app/core/services/channel.service.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Channel } from "../models/channel.model";
+import { ChannelFetcher } from "./fetchers/channel.fetcher";
 
 @Injectable({
   providedIn: 'root'
 })
 
 /**
- * Service class for handling channel-related operations.
+ * Service class for handling channel-related operations using the ChannelFetcher service.
  */
 export class ChannelService {
 
-  // Constructor
-  constructor() {}
+  // Constructor with ChannelFetcher
+  constructor(private http:ChannelFetcher) {}
+
+  private channels : Channel[] = [];
+
+  getChannelByID(id: number) {
+    return this.http.getChannelById(id);
+  }
+
+  postChannel(channel: Channel) {
+    return this.http.postChannel(channel);
+  }
+
+  deleteChannel(id: number) {
+    return this.http.deleteChannel(id);
+  }
+
+  updateChannel(channel: Channel) {
+    return this.http.updateChannel(channel);
+  }
 
 }

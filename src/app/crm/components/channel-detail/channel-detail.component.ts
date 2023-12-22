20a49fa5aa0c561f9c95f5d6ee81d4a1a8c23934
MateoBrunetti <mateo.brunetti42@orange.fr>
2023-12-22 12:14:37 +0100

    Display existing messages of current channel

--- src/app/crm/components/channel-detail/channel-detail.component.ts
+++ src/app/crm/components/channel-detail/channel-detail.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Channel} from "../../../core/models/channel.model";
 import {ChannelService} from "../../../core/services/channel.service";
 import {NgFor} from "@angular/common";
+import {Message} from "../../../core/models/message.model";
 
 @Component({
   selector: 'app-channel-detail',
@@ -17,6 +18,9 @@ export class ChannelDetailComponent {
   @Input()
   channel!: Channel;
 
+  @Output()
+  channelClicked = new EventEmitter<number>();
+
   listChannels : Channel[] = [];
 
   constructor(public channelService : ChannelService) {
@@ -24,5 +28,11 @@ export class ChannelDetailComponent {
       this.listChannels = response;
     })
   }
+
+  switchChannel(id: number): void {
+    this.channelClicked.emit(id);
+  }
+
+
 
 }
